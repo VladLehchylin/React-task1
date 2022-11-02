8eb05eb31998cd2e67f4c0b7d2d1a57eab623852
Vlad <lehchylin.vlad@gmail.com>
2022-11-02 17:36:43 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import info from "./info.json";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <main className="App">
+      <h1>About Me</h1>
+      <p>My name is {info.name}</p>
+      <h2>My skils</h2>
+      <ul>
+        {info.skils.map((item) => (
+          <li className="skils-item">
+            <p>{item.name}</p>
+            <img className="icon" src={item.icon} alt="icon" />
+          </li>
+        ))}
+      </ul>
+      <h2>Work experience</h2>
+      <p>
+        I worked as a {info.workingExp[0]} for {info.workingExp[1]} years.
+      </p>
+      <h2>Just a little bit about my hobbies</h2>
+      <ul>
+        {info.hobbies.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
